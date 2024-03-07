6ae7eef5f96bfb01aaf33f84c6155f504597eb1f
Jacob <107528288+aiBotts@users.noreply.github.com>
2024-03-06 21:09:38 -0600

    Add files via upload

---
+++ application_calc_area_rectangle.py
@@ -0,0 +1,7 @@
+# Calculate area of a rectangle
+length = int(input('Enter the length of the room: ' ))
+width = int(input('Enter the width of the room: ' ))
+area = (length * width)
+
+# Print area
+print('The area is', area)
